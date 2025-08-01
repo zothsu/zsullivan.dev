f8da1d16f02dfd408cc0e972185c4517b3cf5bd9
Zoe Sullivan <70442338+zothsu@users.noreply.github.com>
2025-08-01 08:32:02 -0700

    update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://zsullivan.dev",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
