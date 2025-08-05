f5b61e1e468c7664a88da3ca0d2ad420b50f523a
Zoe Sullivan <70442338+zothsu@users.noreply.github.com>
2025-08-05 13:09:56 -0700

    adds features to static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { compileImageService } from "astro/assets/services/compile";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zsullivan.dev",
-  image: {
-    service: compileImageService, // Use compile-time image optimization
-  },
-  },
+  integrations: [mdx(), sitemap()],
 });
 
