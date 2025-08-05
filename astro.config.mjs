07b1450679502d41004fe0cde5d394dc738916b9
Zoe Sullivan <70442338+zothsu@users.noreply.github.com>
2025-08-05 13:04:53 -0700

    changes from ssr to static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-import cloudflare from "@astrojs/cloudflare";
+import { compileImageService } from "astro/assets/services/compile";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zsullivan.dev",
-  integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  image: {
+    service: compileImageService, // Use compile-time image optimization
+  },
+  },
 });
 
